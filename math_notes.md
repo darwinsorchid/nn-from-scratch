d7f0d54db4998a08049c0f86584c4bc8f6d541d5
Alex <141815742+darwinsorchid@users.noreply.github.com>
2026-02-05 15:00:09 +0200

    Forward propagation, loss and accuracy functions

--- math_notes.md
+++ math_notes.md
@@ -1,9 +1,19 @@
-## Activation Function Formulas
+## Formulas
 
 ### ReLU (forward propagation)
 
-$$f(x) = max(1,x)$$
+$$
+f(x) = max(1,x)
+$$
 
 ### Sigmoid (forward propagation)
 
-$$f(x) = \frac{1}{1 + e^{-x}}$$
+$$
+f(x) = \frac{1}{1 + e^{-x}}
+$$
+
+### Binary Cross-Entropy (BCE) Loss Formula
+
+$$
+\mathrm{BCE} = -\frac{1}{n} \sum_{i=1}^{n} [y\log_{\hat{y}} + (1 - y)\log_{(1- \hat{y})}]
+$$
---
+++ src/models/nn.py
@@ -0,0 +1,198 @@
+import numpy as np
+import logging
+
+# ------------------------------------------ Initialization --------------------------------------------------
+# Configure logging and initialize logger
+logging.basicConfig(
+    filename="logs/init.log",
+    filemode="w",
+    level=logging.DEBUG,
+    format="%(asctime)s | %(levelname)s | %(message)s"
+)
+
+logger = logging.getLogger(__name__)
+
+
+
+# Initialize architecture
+nn_architecture = [
+    {"input_dim": 2, "output_dim": 4, "activation": "relu"},
+    {"input_dim": 4, "output_dim": 6, "activation": "relu"},
+    {"input_dim": 6, "output_dim": 6, "activation": "relu"},
+    {"input_dim": 6, "output_dim": 4, "activation": "relu"},
+    {"input_dim": 4, "output_dim": 1, "activation": "sigmoid"}
+]
+
+
+def init_layers(nn_architecture, seed=42):
+    '''Initialize layers and parameter values per layer: 
+        • weight matrix (output_dim, input_dim)
+        • bias vector   (output_dim, 1)
+    Returns params dict and logs initial parameters to init.log file.
+    
+    :param nn_architecture: Network architecture as a list of dictionaries for each layer of the net.
+    :param seed: Set seed 
+    '''
+
+    # Set seed and initialize params dict
+    np.random.seed(seed)
+    params = {}
+
+    for idx, layer in enumerate(nn_architecture):
+        layer_idx = idx + 1
+        input_dim = layer['input_dim']
+        output_dim = layer['output_dim']
+
+        # Populate params with weight matrix of (output, input) dimensions and bias vector of (output, 1) dimensions
+        params[f"W{layer_idx}"] = np.random.randn(output_dim, input_dim) * 0.1
+        params[f"b{layer_idx}"] = np.random.randn(output_dim, 1) * 0.1
+
+
+    # More readable output
+    np.set_printoptions(
+        precision = 4,
+        suppress = True,
+        linewidth = 120
+    )
+    # Log initial parameters to file
+    logger.debug("Parameter Values:\n %s", params)
+
+    return params
+
+
+# -------------------------------- Activations Functions ---------------------------------
+
+def relu(Z):
+    return np.maximum(0, Z)
+
+def sigmoid(Z):
+    return 1 / (1 + np.exp(-Z))
+
+def relu_backprop():
+    pass
+
+def sigmoid_backprop():
+    pass
+
+def single_forward_propagation(A_prev, W_curr, b_curr, activation="relu"):
+    '''
+    Forward propagation for a single layer.
+    
+    :param A_prev: Activation vector of previous layer.
+    :param W_curr: Weight matrix of current layer.
+    :param b_curr: Bias vector of current layer.
+    :param activation: Activation function of current layer.
+    '''
+    # Calculate Z_curr
+    Z_curr = np.dot(W_curr, A_prev) + b_curr
+
+    if activation == "relu":
+        act_func = relu
+    elif activation == "sigmoid":
+        act_func = sigmoid
+    else:
+        raise Exception('Non-valid activation function.')
+    
+    # Return calculated activation A and intermediate output matrix Z
+    return act_func(Z_curr), Z_curr
+
+
+def full_forward_propagation(X, params, nn_architecture):
+    '''
+    Iterates through all network layers and does forward propagation.
+    
+    :param X: Input vector (activation of layer 0)
+    :param nn_architecture: Network architecture
+    '''
+    # Initialize temporary memory to store params and outputs - to be used later for backward prop.
+    memory = {}
+
+    # Activation of layer 0
+    A_curr = X
+
+    # Iterate through layers
+    for idx, layer in enumerate(nn_architecture):
+        layer_idx = idx + 1
+        # Transfer activation from previous iteration
+        A_prev = A_curr
+
+        # Get weight matrix, bias vector and activation function of current layer
+        W_curr = params[f"W{layer_idx}"]
+        b_curr = params[f"b{layer_idx}"]
+        act_func = layer["activation"]
+        
+        # Do forward propagation for current layer
+        A_curr, Z_curr = single_forward_propagation(A_prev, W_curr, b_curr, act_func)
+
+        # Store intermediate values
+        memory[f"A{idx}"] = A_prev
+        memory[f"Z{layer_idx}"] = Z_curr
+
+    # Return prediction vector and memory of intermediate prediction probabilities
+    return A_curr, memory
+
+
+
+def loss_function(y_hat, y):
+    '''
+    Calculate loss function.
+    Binary Cross-Entropy (BCE) loss for binary classification problems.
+    
+    :param y_hat: Vector of network prediction probabilities
+    :param y: Vector of actual labels.
+    '''
+    # Number of examples
+    n = y_hat.shape[1]
+
+    # Calculate loss according to BCE formula
+    bce = - 1 / n * (np.dot(y, np.log(y_hat).T)) + np.dot(1-y, np.log(1-y_hat).T)
+
+    return np.squeeze(bce)
+
+
+def prob_to_label(probs):
+    '''
+    Turns predicted probabilities to binary labels 0 / 1.
+
+    :param probs: Vector containing predicted probabilities.
+    '''
+    probs_ = np.copy(probs)
+
+    # Threshold probabilities and turn to binary labels.
+    probs_[probs_ > 0.5] = 1
+    probs_[probs_ <= 0.5] = 0 
+
+    return probs_
+
+
+def accuracy(y_hat, y):
+    '''
+    Get model accuracy:
+    1) Element-wise comparison of predicted vs true labels
+    2) Get a True/False value for each column
+    3) Get mean value = Accuracy
+
+    :param y_hat: Predicted probability vector
+    :param y: Real labels
+    '''
+    # Turn predicted probabilities to binary labels
+    y_hat_ = prob_to_label(y_hat)
+
+    # Return accuracy
+    return (y_hat_ == y).all(axis=0).mean()
+
+
+
+def single_backprop():
+    pass
+
+def full_backprop():
+    pass
+
+def update_params():
+    pass
+
+
+if __name__ == "__main__":
+
+    params = init_layers(nn_architecture)
